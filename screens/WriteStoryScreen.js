ccdd3c24c7998cc8ea4d8ebc308d27b7ad7a6277
AgnikaSunil <agnika.sunil@gmail.com>
2020-11-02 09:58:36 +0530

    C71 Project

--- screens/WriteStoryScreen.js
+++ screens/WriteStoryScreen.js
@@ -1,50 +1,88 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Image, ScrollView} from 'react-native';
 import { Header } from 'react-native-elements';
 import { TextInput } from 'react-native-gesture-handler';
 
+import db from '../config'
+import firebase from 'firebase';
+
 export default class WriteStoryScreen extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            title: '',
+            author: '',
+            story: ''
+        }
+    }
+
+    submitStory =()=>{
+        db.collection("stories").add({
+          'title': this.state.title,
+          'author': this.state.author,
+          'story': this.state.story,
+          'date':firebase.firestore.Timestamp.now().toDate(),
+        })
+    }
+
    render(){
        return(
-           <View>
-                <Header
-                    backgroundColor= {'#3ec1d3'}
-                    centerComponent= {{
-                        text: 'Story Hub',
-                        style: { color: '#f6f7d7', fontSize: 20, fontWeight: 'bold' },
-                    }}
-                />
-
-                <View style= {styles.container}>
-                    <View style={styles.inputView}>
-                        <TextInput 
-                            style= {styles.inputBox} 
-                            placeholder= "Story Title"
+           <ScrollView>
+                <View>
+                        <Header
+                            backgroundColor= {'#3ec1d3'}
+                            centerComponent= {{
+                                text: 'Story Hub',
+                                style: { color: '#f6f7d7', fontSize: 20, fontWeight: 'bold' },
+                            }}
                         />
-                    </View>
-                   
-                    <View style={styles.inputView}>
-                        <TextInput 
-                            style= {styles.inputBox} 
-                            placeholder= "Author"
-                        />
-                    </View>
-                   
-                    <View style={styles.inputView}>
-                        <TextInput 
-                            style= {[styles.storyInputBox,{textAlignVertical: "center"}]} 
-                            placeholder= "Write your story" 
-                            multiline= {true}
-                        /> 
-                    </View> 
 
-                    <View style={styles.inputView}>
-                        <TouchableOpacity style= {styles.button}>
-                            <Text style= {styles.buttonText}>Submit</Text>
-                        </TouchableOpacity>
-                    </View>
+                        <View style= {styles.container}>
+                            <View style={styles.inputView}>
+                                <TextInput 
+                                    style= {styles.inputBox} 
+                                    placeholder= "Story Title"
+                                    onChangeText={text => {
+                                        this.setState({ title: text });
+                                    }}
+                                    value={this.state.title}
+                                />
+                            </View>
+                        
+                            <View style={styles.inputView}>
+                                <TextInput 
+                                    style= {styles.inputBox} 
+                                    placeholder= "Author"
+                                    onChangeText={text => {
+                                        this.setState({ author: text });
+                                    }}
+                                    value={this.state.author}
+                                />
+                            </View>
+                        
+                            <View style={styles.inputView}>
+                                <TextInput 
+                                    style= {[styles.storyInputBox,{textAlignVertical: "center"}]} 
+                                    placeholder= "Write your story" 
+                                    multiline= {true}
+                                    onChangeText={text => {
+                                        this.setState({ story: text });
+                                    }}
+                                    value={this.state.story}
+                                /> 
+                            </View> 
+
+                            <View style={styles.inputView}>
+                                <TouchableOpacity 
+                                    style= {styles.button} 
+                                    onPress={this.submitStory}
+                                >
+                                    <Text style= {styles.buttonText}>Submit</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
                 </View>
-           </View>
+           </ScrollView>
        )
    }
 }
@@ -64,17 +102,17 @@ const styles = StyleSheet.create({
     },
     inputView:{
         flexDirection: 'row',
-        margin: 30
+        margin: 10
     },
     inputBox:{
-        marginTop: 200,
+        marginTop: 30,
         width: 300,
         height: 40,
         borderWidth: 1,
         fontSize: 20
     },
     storyInputBox:{
-        marginTop: 200,
+        marginTop: 30,
         width: 300,
         height: 200,
         borderWidth: 1,
@@ -85,6 +123,6 @@ const styles = StyleSheet.create({
         width: 100,
         height: 40,
         borderWidth: 1.5,
-        marginTop: 400
+        marginTop: 70
     }
 });
